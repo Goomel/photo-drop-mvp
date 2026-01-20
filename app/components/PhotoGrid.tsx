fccc9d13539b11531970c20d3c6d24ab4e9725fb
Goomel <thegomel10@gmail.com>
2026-01-20 22:11:08 +0100

    Install and config prettier

--- app/components/PhotoGrid.tsx
+++ app/components/PhotoGrid.tsx
@@ -1,21 +1,17 @@
-'use client';
+"use client";
 
-export default function PhotoGrid({files}: {files: File[]}){
-    return(
-<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-10">
-  {files.map((file, index) => {
-    const previewUrl = URL.createObjectURL(file);
+export default function PhotoGrid({ files }: { files: File[] }) {
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-10">
+      {files.map((file, index) => {
+        const previewUrl = URL.createObjectURL(file);
 
-    return (
-      <div
-        key={index}
-        className="relative w-full aspect-square rounded-lg overflow-hidden"
-      >
-        <img className="w-full h-full object-cover" key={index} src={previewUrl} alt={file.name} />
-      </div>
-    );
-  })}
-</div>
-
-    )
+        return (
+          <div key={index} className="relative w-full aspect-square rounded-lg overflow-hidden">
+            <img className="w-full h-full object-cover" key={index} src={previewUrl} alt={file.name} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-darkGray-400 font-sans py-10 lg:py-20">
       <main className="w-full container mx-auto px-5 lg:px-10 ">
-        <FileUploader/>
+        <FileUploader />
       </main>
     </div>
   );
